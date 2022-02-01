2e31e46286b0435aad68db5c0cd38e2cca3f1d4b
antonkrutikov <kap@reso-med.com>
2022-02-02 00:49:52 +0300

    init express api

---
+++ README.md
@@ -0,0 +1,18 @@
+# Extract URLs from Google Docs json document
+
+Small express app with 2 api endpoints, which accept json as POST body and retunr array of extracted urls.
+
+## /urls
+```
+curl -X POST -H "Content-Type: application/json" -d @input.json http://localhost:3000/urls
+
+["http://youtube.com","http://google.com"]
+```
+## /urls_with_text
+```
+curl -X POST -H "Content-Type: application/json" -d @input.json http://localhost:3000/urls_with_text
+
+[{"text":"Компания","url":"http://youtube.com"},{"text":"здесь","url":"http://google.com"}]`
+```
+
+
---
+++ app.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const express = require('express')
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const port = 3000;
+const max_body_size_mb = 10
+
+// Configuring body parser middleware
+app.use(bodyParser.json({limit: `${max_body_size_mb}mb`}));
+
+app.post('/urls_with_text', (req, res) => {
+    const doc = req.body;
+    links = findURL(doc)
+    res.send(links);
+});
+
+app.post('/urls', (req, res) => {
+    const doc = req.body;
+    links = findURL(doc).map(x => x.url)
+    res.send(links);
+});
+
+app.listen(port, () => console.log(`URL extracter start on port ${port}!`));
+
+
+// Find all text parts with link textStyle and return text and url
+const findURL = (obj, results = []) => {
+    const r = results;
+    Object.keys(obj).forEach(key => {
+       const value = obj[key];
+       if ((key === 'textRun' || key === 'inlineObjectElement') && value.hasOwnProperty('textStyle') && value.textStyle.hasOwnProperty('link')) {
+            r.push({text: value.content ?? null, url: value.textStyle.link.url})
+       }
+       if(typeof value === 'object'){
+          findURL(value, r);
+       }
+    });
+    return r;
+ };
